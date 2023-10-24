a87099925aa60799b35d7901556517ba95278f0c
Sabbirahmed0007 <>
2023-10-24 19:00:58 +0600

    updated document

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    client.connect();
+    // client.connect();
 
     const userCollection= client.db('userDB').collection('users');
     const dataCollection= client.db('userDB').collection('data');
@@ -77,7 +77,7 @@ async function run() {
       res.send(result);
   })
 
-  app.put('/data/:id', async(req, res)=>{
+  app.put('/singledata/:id', async(req, res)=>{
     const id= req.params.id;
     console.log(id);
     const filter= {_id: new ObjectId(id)};
@@ -138,8 +138,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    // client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
